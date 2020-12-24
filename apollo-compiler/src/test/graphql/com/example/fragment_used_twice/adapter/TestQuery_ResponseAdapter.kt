1058510c5e95a5968eaf4b07aeeff23709b5bf26
Martin Bonnin <martin@mbonnin.net>
2020-12-24 14:38:28 +0100

    Generate CustomScalar as a data class

--- apollo-compiler/src/test/graphql/com/example/fragment_used_twice/adapter/TestQuery_ResponseAdapter.kt
+++ apollo-compiler/src/test/graphql/com/example/fragment_used_twice/adapter/TestQuery_ResponseAdapter.kt
@@ -5,12 +5,13 @@
 //
 package com.example.fragment_used_twice.adapter
 
+import com.apollographql.apollo.api.CustomScalar
 import com.apollographql.apollo.api.ResponseField
 import com.apollographql.apollo.api.internal.ResponseAdapter
 import com.apollographql.apollo.api.internal.ResponseReader
 import com.apollographql.apollo.api.internal.ResponseWriter
 import com.example.fragment_used_twice.TestQuery
-import com.example.fragment_used_twice.type.CustomScalar
+import com.example.fragment_used_twice.type.DATE
 import kotlin.Any
 import kotlin.Array
 import kotlin.String
@@ -90,7 +91,7 @@ object TestQuery_ResponseAdapter : ResponseAdapter<TestQuery.Data> {
         private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
           ResponseField.forString("__typename", "__typename", null, false, null),
           ResponseField.forString("name", "name", null, false, null),
-          ResponseField.forCustomScalar("birthDate", "birthDate", null, false, CustomScalar.Date, null)
+          ResponseField.forCustomScalar("birthDate", "birthDate", null, false, CustomScalar.DATE, null)
         )
 
         override fun fromResponse(reader: ResponseReader, __typename: String?):
@@ -126,7 +127,7 @@ object TestQuery_ResponseAdapter : ResponseAdapter<TestQuery.Data> {
         private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
           ResponseField.forString("__typename", "__typename", null, false, null),
           ResponseField.forString("name", "name", null, false, null),
-          ResponseField.forCustomScalar("birthDate", "birthDate", null, false, CustomScalar.Date, null)
+          ResponseField.forCustomScalar("birthDate", "birthDate", null, false, CustomScalar.DATE, null)
         )
 
         override fun fromResponse(reader: ResponseReader, __typename: String?):
